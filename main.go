50e888a2e002d19cf982aab5ff0c41fe323e69f1
Bappa Saha <bappasaha161@gmail.com>
2025-06-07 19:18:26 +0600

    09-Refctoring code and route for scalability and maintalibility

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
-	"strconv"
-
 	"bappa.com/rest/db"
-	"bappa.com/rest/models"
+	"bappa.com/rest/routes"
 	"github.com/fatih/color"
 	"github.com/gin-gonic/gin"
 )
@@ -16,85 +12,13 @@ func main() {
 	db.InitDB()                // first start the database
 	var server = gin.Default() //start the server
 
-	server.GET("/events", getEvents)
-	server.GET("/events/:id",getEvent) 
-	server.POST("/events", createEvent)
+	//register all routes
+	routes.RegisterRoute(server)
+
 
 	color.Cyan("🔋 🚀 Server running at http://localhost:8080")
 	server.Run(":8080")
 
 }
 
-func getEvents(context *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{
-			"message": "Could not fetch events",
-		})
-		return // other wise below code will not executed
-	}
 
-	// Convert events to pretty JSON
-	jsonOutput, _ := json.MarshalIndent(events, "", "  ")
-	color.Green("📦 Retrieved Events:\n%s", string(jsonOutput))
-
-	context.JSON(http.StatusOK, events)
-}
-
-func getEvent(context *gin.Context){
-	eventId,err := strconv.ParseInt(context.Param("id"),10,64) 
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{
-			"message": "Could not parse event id",
-		})
-		return // other wise below code will not executed
-	}
-
-	event,err := models.GetEventById(eventId)
-
-	if err != nil {
-		context.JSON(http.StatusInternalServerError,gin.H{
-			"message" : "Could not fetch event",
-		})
-		return
-	}
-
-	// Convert events to pretty JSON
-	jsonOutput, _ := json.MarshalIndent(event, "", "  ")
-	color.Green("📦 Retrieved Events:\n%s", string(jsonOutput))
-
-	context.JSON(http.StatusOK,event)
-}
-
-func createEvent(context *gin.Context) {
-	var event models.Event
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{
-			"message": "Could not parse the request data",
-		})
-		return
-	}
-
-	event.ID = 1     //dummy value
-	event.UserID = 1 //dummy value
-
-	//save event
-	// err =models.Save(event)  // if use func Save(e Event){...})
-	err = event.Save() // if use func(e Event) Save(){...}  // receiver function
-
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{
-			"message": "Could not create event",
-		})
-		return // other wise below code will not executed
-	}
-
-	//also send a response for success case
-	context.JSON(http.StatusCreated, gin.H{
-		"message": "✅ Event Created Successfully",
-		"event":   event,
-	})
-}
---
+++ routes/routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func  RegisterRoute(server *gin.Engine)  {
+
+	//root: http://localhost:8080
+	//event routes
+	server.GET("/events", getEvents)
+	server.GET("/events/:id",getEvent) 
+	server.POST("/events", createEvent)
+}
