3bea3a080290f300ffdf601a7fec352325f5f26d
Bappa Saha <bappasaha161@gmail.com>
2025-06-09 12:59:47 +0600

    21-Middleware integration in routes

--- routes/routes.go
+++ routes/routes.go
@@ -12,16 +12,37 @@ func  RegisterRoute(server *gin.Engine)  {
 
 	//root: http://localhost:8080
 	//event routes
+
+
+	// Best Practice 
+	authenticated := server.Group("/")
+	authenticated.Use(middlewares.Authenticate)
+
+	// events 
 	server.GET("/events", controllers.GetEvents)
 	server.GET("/events/:id",controllers.GetEvent) 
-	server.POST("/events",middlewares.Authenticate, controllers.CreateEvent)
-	server.PUT("/events/:id", controllers.UpdateEvent)
-	server.DELETE("/events/:id", controllers.DeleteEvent)
+	authenticated.POST("/events", controllers.CreateEvent)
+	authenticated.PUT("/events/:id", controllers.UpdateEvent)
+	authenticated.DELETE("/events/:id", controllers.DeleteEvent)
 
-	//users
+		//users
 	server.POST("/signup",controllers.Signup)
 	server.POST("/login",controllers.Login)
 
+
+	//todo:better way
+	// server.POST("/events",middlewares.Authenticate, controllers.CreateEvent)
+	// server.GET("/events", controllers.GetEvents)
+	// server.GET("/events/:id",controllers.GetEvent) 
+	// server.PUT("/events/:id", controllers.UpdateEvent)
+	// server.DELETE("/events/:id", controllers.DeleteEvent)
+
+
+
+
+
+
+	//todo: Old way
 	// server.GET("/events", getEvents)
 	// server.GET("/events/:id",getEvent) 
 	// server.POST("/events", createEvent)
